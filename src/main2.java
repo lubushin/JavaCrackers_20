b4ec77d5ee48e8b3454c66e799d2347855159350
Lyubov Ubushin <Lyubau1124@gmail.com>
2020-11-18 17:04:26 -0500

    Added one more message

--- src/main2.java
+++ src/main2.java
@@ -2,5 +2,7 @@ public class main2 {
     public static void main(String[] args) {
 
         System.out.println("Have a great evening");
+
+        System.out.println("We are doing great.");
     }
 }
