5b26c2629b303d011fa4d893c6ad77f761a473a5
Lyubov Ubushin <Lyubau1124@gmail.com>
2020-11-18 16:48:48 -0500

    message 3

--- src/main.java
+++ src/main.java
@@ -2,9 +2,11 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hi, I'm in group 20.");
 
-        System.out.println("Today we have an awesome class.");
+        System.out.println("Today we had an awesome class.");
 
         System.out.println("I hope everybody had a wonderful day");
+
+        System.out.println("Let's start a class.");
     }
 
 }
