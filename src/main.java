3acfe709a439b54a1f8404414330fca10208b05c
Lyubov Ubushin <Lyubau1124@gmail.com>
2020-11-18 16:46:17 -0500

    message 1

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hi, im in group 20.");
+        System.out.println("Hi, I'm in group 20.");
+
+        System.out.println("Today we have an awesome class.");
     }
 
 }
